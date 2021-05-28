c44dfd4560af0f463f62e68d5a34f56c6c6b4944
` Orchai Blyat! <84289524+Orchais@users.noreply.github.com>
2021-05-28 22:36:27 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -7,10 +7,7 @@ var prefix = ayarlar.prefix;
 module.exports = client => {
 
   client.user.setStatus("dnd");
- client.user.setStatus('idle')
 
-console.log(`[READY]: Bot **${client.user.tag}** olarak giriş yaptı.`)
  client.user.setActivity(`Serendia ❤️ Øʀcaιs`,  { type: `WATCHING` }) // Durumu değiştirmezseniz mutlu olurum ^^
 
 };
-
